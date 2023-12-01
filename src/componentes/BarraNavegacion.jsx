61c0a7816b98dc9862373eb4b55ad7c57920346b
diego-palacio <diego_palacio2015@hotmail.com>
2023-11-30 23:34:55 -0300

    se agrega json de partidos + fix

--- src/componentes/BarraNavegacion.jsx
+++ src/componentes/BarraNavegacion.jsx
@@ -1,3 +1,4 @@
+import { partidos } from "../data/partidos"
 
 export const BarraNavegacion = () => {
   return (
@@ -5,69 +6,19 @@ export const BarraNavegacion = () => {
 
 <div className="partidos">
 
-  
-  <div className="arg-ar"> 
-       <a>ARGENTINA 1 vs ARABIA SAUDITA 2</a>
+  {partidos.map((partido) =>
+
+        <div className={partido.clase}> 
+        <a>{partido.versus}</a>
           <div className="flex-data"> 
               <img src="src/imagenes/arg.png" alt=""/>
-              <img src="src/imagenes/ar.png" alt=""/>
+              <img src={partido.imagen} alt=""/>
           </div>
           <input type="button" value="Ver video" />
-  </div>
-
-  <div className="arg-mex"> 
-       <a>ARGENTINA 2 vs MEXICO 0</a>
-          <div className="flex-data"> 
-            <img src="src/imagenes/arg.png" alt=""/>
-            <img src="src/imagenes/mex.png" alt=""/>
-          </div>
-          <input type="button" value="Ver video" />
-  </div>
-
-  <div className="arg-pol"> 
-       <a>ARGENTINA 2 vs POLONIA 0</a>
-          <div className="flex-data"> 
-            <img src="src/imagenes/arg.png" alt=""/>
-            <img src="src/imagenes/pol.png" alt=""/>
-          </div>
-          <input type="button" value="Ver video" />
-  </div>
-
-  <div className="octavos">
-       <a>ARGENTINA 2 vs AUSTRALIA 1</a>
-          <div className="flex-data"> 
-            <img src="src/imagenes/arg.png" alt=""/>
-            <img src="src/imagenes/aus.png" alt=""/>
-          </div>
-          <input type="button" value="Ver video" />
-  </div>
-
-  <div className="cuartos">
-      <a>ARGENTINA 2(4) vs PAISES BAJOS 2(3)</a>
-        <div className="flex-data"> 
-          <img src="src/imagenes/arg.png" alt=""/>
-          <img src="src/imagenes/hol.png" alt=""/>
         </div>
-        <input type="button" value="Ver video" />
-  </div>
 
-  <div className="semifinal">
-      <a>ARGENTINA 3 vs CROACIA 0</a>
-        <div className="flex-data"> 
-          <img src="src/imagenes/arg.png" alt=""/>
-          <img src="src/imagenes/cro.png" alt=""/>
-        </div>  
-        <input type="button" value="Ver video" />
-  </div>
+  )}
 
-  <div className="final">
-       <a>ARGENTINA 3(4) vs FRANCIA 3(2)</a>
-        <div className="flex-data"> 
-           <img src="src/imagenes/arg.png" alt=""/>
-           <img src="src/imagenes/fra.png" alt=""/>
-        </div>
-        <input type="button" value="Ver video" />
-  </div>
 </div>
 
     </section>    
--- src/data/partidos.js
+++ src/data/partidos.js
@@ -1,16 +1,40 @@
 export const partidos = [
 
     {
-        nombre:"diego",
-        apellido:"palacio"
-
+        versus:"ARGENTINA VS ARABIA SAUDITA",
+        imagen:"src/imagenes/ar.png",
+        clase:"arg-ar"
     }
     ,
     {
-        nombre:"2",
-        apellido:"sada"
-
+        versus:"ARGENTINA VS MEXICO",
+        imagen:"src/imagenes/mex.png",
+        clase:"arg-mex"
+    },
+    {
+        versus:"ARGENTINA VS POLONIA",
+        imagen:"src/imagenes/pol.png",
+        clase:"arg-pol"
+    },
+    {
+        versus:"ARGENTINA VS AUSTRALIA",
+        imagen:"src/imagenes/aus.png",
+        clase:"octavos"
+    },
+    {
+        versus:"ARGENTINA VS PAISES BAJOS",
+        imagen:"src/imagenes/hol.png",
+        clase:"cuartos"
+    },
+    {
+        versus:"ARGENTINA VS CROACIA",
+        imagen:"src/imagenes/cro.png",
+        clase:"semifinal"
+    },
+    {
+        versus:"ARGENTINA VS FRANCIA",
+        imagen:"src/imagenes/fra.png",
+        clase:"final"
     }
-
 
 ]
