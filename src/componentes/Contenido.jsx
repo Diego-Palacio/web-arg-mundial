4841ae2e28b4edbca5a49bf24092bf80dcf549aa
diego-palacio <diego_palacio2015@hotmail.com>
2023-12-01 00:07:57 -0300

    agrego dep react router

--- src/componentes/Contenido.jsx
+++ src/componentes/Contenido.jsx
@@ -2,7 +2,7 @@
 export const Contenido = () => {
   return (
     <div>
-       <iframe src="https://www.tokyvideo.com/es/embed/212520" width="640" height="360" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" allowtransparency="true" style={{display:"block",maxWidth:"100%",margin:"0 auto 10px"}}></iframe>
+       <iframe src="https://www.tokyvidxeo.com/es/embed/212520" width="640" height="360" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" allowtransparency="true" style={{display:"block",maxWidth:"100%",margin:"0 auto 10px"}}></iframe>
     </div>
 
     
