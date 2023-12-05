8188a41606ef8fe0312b0bb0971b1b33731c3b0d
diego-palacio <diego_palacio2015@hotmail.com>
2023-12-04 21:47:19 -0300

    fix

--- src/componentes/Goles.jsx
+++ src/componentes/Goles.jsx
@@ -10,26 +10,24 @@ export const Goles = () => {
 
             {jugadores.map(jugador=>
             
-               <div className={jugador.clase}> 
-               
-                    <div id="si">
+                    <div className={jugador.clase}> 
+                    
+                        <div id="container-cards">
 
-                    <div id="container-fondo-jug">
+                            <div id="container-fondo-jug" style={{backgroundImage:`url(${jugador.imagen})`}}>
 
+                            </div>
 
-</div>
-
-<div id="container-flex-jug"> 
-        <div id="jugador-versus"><h3>{jugador.versus}</h3></div>
-        <div id="container-min-gol">
-            <h3> Min:{jugador.minuto} </h3>
-            <button id="boton-ver-gol"><a>VER GOL</a></button> 
-        </div>
-</div>
-
+                            <div id="container-flex-jug"> 
+                                    <div id="jugador-versus"><h3 id="versus">{jugador.versus}</h3></div>
+                                    <div id="container-min-gol">
+                                        <h2> {jugador.jugador} </h2>
+                                        <h3> Min:{jugador.minuto} </h3>
+                                        <button id="boton-ver-gol"><a>VER GOL</a></button> 
+                                    </div>
+                            </div>
+                        </div>
                     </div>
-
-                </div>
             )}
 
 
