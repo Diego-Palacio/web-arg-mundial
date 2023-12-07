98abddd34c61b56d25b57b5614f91b0bc00cb0ba
diego-palacio <diego_palacio2015@hotmail.com>
2023-12-07 17:05:43 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:"/web-arg-mundial/",
-})
+  esbuild:{
+    loader:'jsx',
+  },
+  optimizeDeps:{
+    esbuildOptions:{
+      loader:{
+        '.js':'jsx',
+      },
+    },
+  },
+})  
