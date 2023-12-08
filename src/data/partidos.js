6cf9bc2a6dc8c96f192335ac239837725267f3ae
diego-palacio <diego_palacio2015@hotmail.com>
2023-12-08 16:03:48 -0300

    cambio de lugar carpeta imagenes

--- src/data/partidos.js
+++ src/data/partidos.js
@@ -2,44 +2,44 @@ export const partidos = [
 
     {
         versus:"ARGENTINA VS ARABIA SAUDITA",
-        imagen:"src/imagenes/ar.png",
+        imagen:"imagenes/ar.png",
         clase:"arg-ar",
         video:"/212108"
     }
     ,
     {
         versus:"ARGENTINA VS MEXICO",
-        imagen:"src/imagenes/mex.png",
+        imagen:"imagenes/mex.png",
         clase:"arg-mex",
         video:"/212427"
     },
     {
         versus:"ARGENTINA VS POLONIA",
-        imagen:"src/imagenes/pol.png",
+        imagen:"imagenes/pol.png",
         clase:"arg-pol",
         video:"/212520"
     },
     {
         versus:"ARGENTINA VS AUSTRALIA",
-        imagen:"src/imagenes/aus.png",
+        imagen:"imagenes/aus.png",
         clase:"octavos",
         video:"/212876"
     },
     {
         versus:"ARGENTINA VS PAISES BAJOS",
-        imagen:"src/imagenes/hol.png",
+        imagen:"imagenes/hol.png",
         clase:"cuartos",
         video:"/212939"
     },
     {
         versus:"ARGENTINA VS CROACIA",
-        imagen:"src/imagenes/cro.png",
+        imagen:"imagenes/cro.png",
         clase:"semifinal",
         video:"/213072"
     },
     {
         versus:"ARGENTINA VS FRANCIA",
-        imagen:"src/imagenes/fra.png",
+        imagen:"imagenes/fra.png",
         clase:"final",
         video:"/194824"
     }
