04abd6e5b3376078c2bc65ba67c54003697652ec
diego-palacio <diego_palacio2015@hotmail.com>
2023-12-08 20:20:21 -0300

    cambio color fondo cards

--- src/componentes/Contenido.jsx
+++ src/componentes/Contenido.jsx
@@ -6,7 +6,12 @@ export const Contenido = ({url}) => {
    
         <iframe src= { `https://www.tokyvideo.com/es/embed/${url}` } width="640" height="360" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" allowtransparency="true" style={{display:"block",maxWidth:"100%",margin:"0 auto 10px"}}></iframe>
         <button id="volver"><a id="volver-a"href="/partidos">VOLVER ATRAS</a></button> 
-     
+        <br /><br />
+        <br /><br />
+        <br /><br />
+        <br /><br />
+        <br /><br />
+        <br /><br />
     </div>
 
 
