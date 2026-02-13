d59a215db47a9b4706b81435d5e75450383b2e81
Diego palacio <diego_palacio2015@hotmail.com>
2026-02-13 00:50:36 -0300

    se corrigio error  de ruta a pagina de inicio y es margenes

--- src/componentes/Contenido.jsx
+++ src/componentes/Contenido.jsx
@@ -6,7 +6,7 @@ export const Contenido = ({url}) => {
    
        {/*  <iframe src= { `https://www.tokyvideo.com/es/embed/${url}` } width="640" height="360" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" allowtransparency="true" style={{display:"block",maxWidth:"100%",margin:"0 auto 10px"}}></iframe>*/}
          <iframe src= { `https://archive.org/download/${url}` } width="640" height="360" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" allowtransparency="true" style={{display:"block",maxWidth:"100%",margin:"0 auto 10px"}}></iframe> 
-        <button id="volver"><a id="volver-a"href="/web-arg-mundial/partidos">VOLVER ATRAS</a></button> 
+        <button id="volver"><a id="volver-a"href="/web-arg-mundial/#/partidos">VOLVER ATRAS</a></button>
         <br /><br />
         <br /><br />
         <br /><br />
