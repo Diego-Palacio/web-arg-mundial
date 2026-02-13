c4e706698504aa3bc98d8c763befefa759686289
Diego palacio <diego_palacio2015@hotmail.com>
2026-02-12 22:30:11 -0300

    se agregan nuevas url de videos

--- src/data/partidos.js
+++ src/data/partidos.js
@@ -13,6 +13,7 @@ export const partidos = [
         imagen:argentina,
         clase:"arg-ar",
         //video:"/212108"
+        video:"https://archive.org/details/argentinavsarabia2022"
     }
     ,
     {
@@ -20,36 +21,42 @@ export const partidos = [
         imagen:mexico,
         clase:"arg-mex",
        // video:"/212427"
+       video:"https://archive.org/details/argentinavsmexico2022"
     },
     {
         versus:"ARGENTINA VS POLONIA",
         imagen:polonia,
         clase:"arg-pol",
        // video:"/212520"
+       video:"https://archive.org/details/poloniavsargentina2022"
     },
     {
         versus:"ARGENTINA VS AUSTRALIA",
         imagen:australia,
         clase:"octavos",
        // video:"/212876"
+        video: "https://archive.org/details/argentinavsaustralia2022"
     },
     {
         versus:"ARGENTINA VS PAISES BAJOS",
         imagen:holanda,
         clase:"cuartos",
        // video:"/212939"
+       video:"https://archive.org/details/holandavsargentina2022"
     },
     {
         versus:"ARGENTINA VS CROACIA",
         imagen:croacia,
         clase:"semifinal",
        // video:"/213072"
+       video:"https://archive.org/details/argentinavscroacia2022"
     },
     {
         versus:"ARGENTINA VS FRANCIA",
         imagen:francia,
         clase:"final",
       //  video:"/194824"
+      video:"https://archive.org/details/argentinavsfrancia2022"
     }
 
 ]
